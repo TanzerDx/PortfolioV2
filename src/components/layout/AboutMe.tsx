b18af7ab02ac9d9219a68419fe5737ee60482c8b
TanzerDx <hristoganchev3@gmail.com>
2025-09-10 01:57:55 +0900

    Added descriptions

--- src/components/layout/AboutMe.tsx
+++ src/components/layout/AboutMe.tsx
@@ -1,9 +1,31 @@
 const AboutMe: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-textColor subpixel-antialiased text-md w-[700px]">
-        To fill in text.
-      </h1>
+    <div className="w-[700px] space-y-4">
+      <p className="text-textColor subpixel-antialiased text-lg">
+        Hey! My name is Hristo. I am a 4th-year Software Engineering student in
+        the Netherlands.
+      </p>
+
+      <p className="text-textColor subpixel-antialiased text-lg">
+        I started my software journey in 2022 with little to no experience.
+        Until today, I have learned how to work with many languages, tools, and
+        frameworks that are widely used in the industry. I am serious about my
+        career development, so I am always eager to learn something new and
+        improve my skills.
+      </p>
+
+      <p className="text-textColor subpixel-antialiased text-lg">
+        Currently, I am looking for an internship that will allow me to grow
+        professionally even more and prepare me for the field of software
+        engineering after my graduation. I can work both independently and in a
+        team setting. If you are looking for a young professional, you are in
+        the right place.
+      </p>
+
+      <p className="text-textColor subpixel-antialiased text-lg">
+        In my free time, I enjoy travelling, doing sports, and going out with
+        friends and family.
+      </p>
     </div>
   );
 };
--- src/components/layout/Projects.tsx
+++ src/components/layout/Projects.tsx
@@ -1,12 +1,16 @@
 const Projects: React.FC = () => {
   return (
-    <div className="flex flex-col gap-14 w-[700px]">
+    <div className="flex flex-col gap-8 w-[700px]">
       <div className="group rounded-2xl border border-transparent bg-white/0 hover:bg-white/10 duration-300 px-1 py-4">
         <h1 className="text-textColor font-semibold subpixel-antialiased text-lg group-hover:text-primaryColor duration-300">
           TICKETPASS
         </h1>
-        <h1 className="mt-[1%] text-textColor subpixel-antialiased text-lg">
-          To fill in text.
+        <h1 className="mt-[1%] text-textColor subpixel-antialiased text-md">
+          A user-friendly website where concert lovers can purchase tickets to
+          many events and have all of them in one place. Upon creating a
+          registration, the user immediately has access to purchasing concert
+          tickets, display the e-tickets on their devices, and has history of
+          past concerts they have attended.
         </h1>
         <div className="flex flex-row mt-[2%] gap-5">
           <div className="rounded-3xl pt-[0.5%] pb-[0.5%] pl-[1%] pr-[1%] bg-secondaryColor/25">
@@ -36,8 +40,11 @@ const Projects: React.FC = () => {
         <h1 className="text-textColor font-semibold subpixel-antialiased text-lg group-hover:text-primaryColor duration-300">
           ARCANE DEFENDER
         </h1>
-        <h1 className="mt-[1%] text-textColor subpixel-antialiased text-lg">
-          To fill in text.
+        <h1 className="mt-[1%] text-textColor subpixel-antialiased text-md">
+          A video game that I have worked on in collaboration with another
+          software developer. It is a top-down tower defense game made entirely
+          in Unity. The goal of the player is simple: build defense against
+          numerous waves of monsters and defeat the final boss.
         </h1>
         <div className="flex flex-row mt-[2%] gap-5">
           <div className="rounded-3xl pt-[0.5%] pb-[0.5%] pl-[1%] pr-[1%] bg-secondaryColor/25">
@@ -52,8 +59,13 @@ const Projects: React.FC = () => {
         <h1 className="text-textColor font-semibold subpixel-antialiased text-lg group-hover:text-primaryColor duration-300">
           PIBBLETV
         </h1>
-        <h1 className="mt-[1%] text-textColor subpixel-antialiased text-lg">
-          To fill in text.
+        <h1 className="mt-[1%] text-textColor subpixel-antialiased text-md">
+          A game-oriented livestreaming platform with a variety of different
+          categories which users can choose from. The application allows users
+          to broadcast their screen in real time while playing video games for
+          an audience. For this project, I have used SRT as the streaming
+          protocol and SRS as a streaming server to deliver the best quality
+          possible to the users.
         </h1>
         <div className="flex flex-row mt-[2%] gap-5">
           <div className="rounded-3xl pt-[0.5%] pb-[0.5%] pl-[1%] pr-[1%] bg-secondaryColor/25">
